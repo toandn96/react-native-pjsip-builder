f080ce5b989fdd69630c9bd546feae4cea588dbb
Toan Dao Ngoc <>
2023-09-26 11:12:30 +0700

    feat: update build srcipt

--- build_android.sh
+++ build_android.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -e
-
+echo "Set environment for android!"
 IMAGE_NAME="react-native-pjsip-builder/android"
 CONTAINER_NAME="react-native-pjsip-builder-${RANDOM}"
 
-rm -rf ./dist/android;
-mkdir -p ./dist/;
+echo "Remove android dist folder!"
+rm -rf ./dist/android
+mkdir -p ./dist/
 
+echo "Starting docker buid for android!"
+DOCKER_BUILDKIT=1 docker build --no-cache --progress=plain -t react-native-pjsip-builder/android ./android/
 
-DOCKER_BUILDKIT=0 docker build  --progress=plain -t react-native-pjsip-builder/android ./android/;
+echo "Starting docker run container $CONTAINER_NAME for android!"
 docker run --name ${CONTAINER_NAME} ${IMAGE_NAME} bin/true
 
+echo "Starting docker cp container $CONTAINER_NAME"
 docker cp ${CONTAINER_NAME}:/dist/android ./dist/android
 
+echo "Docker rm $CONTAINER_NAME"
 docker rm ${CONTAINER_NAME}
